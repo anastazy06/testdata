4fcae13e4a5a1b5b2cba668f7a4706b305c35587
anastazy06 <114347431+anastazy06@users.noreply.github.com>
2022-12-03 00:05:55 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,73 +1,100 @@
-right_answer_n = 0;
+const span1 = document.getElementById('span1')
+const span2 = document.getElementById('span2')
+const span3 = document.getElementById('span3')
+const span4 = document.getElementById('span4')
 
-k = Number(n_question.value);
-console.log(k);
-n.innerHTML=k+1;
-n_right.innerHTML=right_answer_n;
-point = 1;
+const input1 = document.getElementById('y1')
+const input2 = document.getElementById('y2')
+const input3 = document.getElementById('y3')
+const input4 = document.getElementById('y4')
 
-answers(n_question.value);
+const getAnswerBtn = document.getElementById('get_answer')
+const viewAnswerBtn = document.getElementById('view_answer')
+const nextQuestionBtn = document.getElementById('next_question')
 
-get_answer.addEventListener("click", f_get_answer);
-view_answer.addEventListener("click", f_view_answer);
-next_question.addEventListener("click", f_next_question);
+const inputQuestion = document.getElementById('n_question')
+const questionNumber = document.getElementById('question_number')
+const rigthQuestionNumber = document.getElementById('number_right_questions')
 
-  function f_get_answer (){
+let n_right_answer = document.getElementById('n_right_answer')
 
-    if (y1.checked){ n_a = 1;}
-    if (y2.checked){ n_a = 2;}
-    if (y3.checked){ n_a = 3;}
-    if (y4.checked){ n_a = 4;}
+let right_answer_n = 0
+let point = 1
+let k = Number(inputQuestion.value)
 
-    if (n_a == n_right_answer){
-      right_div.classList.remove("hidden");
-      wrong_div.classList.add("hidden");
-      right_answer_n = right_answer_n +point;
-      point = 0;
-    }else{
-      right_div.classList.add("hidden");
-      wrong_div.classList.remove("hidden");
-      point = 0;
+questionNumber.innerHTML = k + 1
+rigthQuestionNumber.innerHTML = right_answer_n
+
+fetchAnswers(inputQuestion.value)
+
+getAnswerBtn.addEventListener('click', getAnswer)
+viewAnswerBtn.addEventListener('click', viewAnswer)
+nextQuestionBtn.addEventListener('click', nextQuestion)
+
+function fetchAnswers(k) {
+    let query = 'https://innovations.kh.ua/quiz/list/?author_id=71&n=' + k
+
+    fetch(query)
+        .then((response) => response.json())
+        .then(function (quiz) {
+            question.innerHTML = quiz.question_arr[0]
+            title.innerHTML = quiz.title_arr[0]
+            span1.innerHTML = quiz.a1_arr[0]
+            span2.innerHTML = quiz.a2_arr[0]
+            span3.innerHTML = quiz.a3_arr[0]
+            span4.innerHTML = quiz.a4_arr[0]
+            answer.innerHTML = quiz.answer_arr[0]
+            n_right_answer = Number(quiz.n_right_answer_arr[0])
+            inputQuestion.value = quiz.total_n
+            console.log(quiz)
+            console.log(quiz)
+        })
+}
+
+function getAnswer() {
+    if (input1.checked) {
+        n_a = 1
     }
-   
-  }
-  function f_view_answer (){
-    answer.classList.toggle("hidden");
-    view_answer.classList.toggle("opend");
-  }
-  function f_next_question (){
-    if (k<Number(n_question.value)-1){
-    console.log(k,Number(n_question.value));
-    right_div.classList.add("hidden");
-    answer.classList.add("hidden");
-    point = 1;
-    k+=1;
-    n_right.innerHTML=right_answer_n;
-    n.innerHTML=k+1;
-    answers(k);
-    }else{
-      n_right.innerHTML=right_answer_n;
-      questions.classList.add("hidden");
-
+    if (input2.checked) {
+        n_a = 2
     }
-  }
+    if (input3.checked) {
+        n_a = 3
+    }
+    if (input4.checked) {
+        n_a = 4
+    }
+    if (n_a === n_right_answer) {
+        right_div.classList.remove('hidden')
+        wrong_div.classList.add('hidden')
 
-function answers(k){
-  query = 'https://innovations.kh.ua/quiz/list/?author_id=71&n='+k;
-  console.log(query);
-  fetch(query).then(response => response.json())
-  .then(function (quiz) {
-    question.innerHTML=quiz.question_arr[0];
-    title.innerHTML = quiz.title_arr[0];
-    a1.innerHTML = quiz.a1_arr[0];
-    a2.innerHTML = quiz.a2_arr[0];
-    a3.innerHTML = quiz.a3_arr[0];
-    a4.innerHTML = quiz.a4_arr[0];
-    answer.innerHTML = quiz.answer_arr[0];
-    
-    n_right_answer = quiz.n_right_answer_arr[0];
-    n_question.value = quiz.total_n;
-    console.log(quiz);
-      
-  });
+        right_answer_n = right_answer_n + point
+        point = 0
+    } else {
+        right_div.classList.add('hidden')
+        wrong_div.classList.remove('hidden')
+
+        point = 0
+    }
+}
+
+function viewAnswer() {
+    answer.classList.toggle('hidden')
+    view_answer.classList.toggle('opend')
+}
+
+function nextQuestion() {
+    if (k < Number(inputQuestion.value) - 1) {
+        right_div.classList.add('hidden')
+        answer.classList.add('hidden')
+        point = 1
+        k += 1
+        rigthQuestionNumber.innerHTML = right_answer_n
+        questionNumber.innerHTML = k + 1
+        fetchAnswers(k)
+    } else {
+        rigthQuestionNumber.innerHTML = right_answer_n
+
+        questions.classList.add('hidden')
+    }
 }
